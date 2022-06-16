83e2ce4c77667ea75086335bc146abb6be73f760
FahadKS <74481173+fahad-ks@users.noreply.github.com>
2022-06-16 18:56:04 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,24 @@
+print("welcome to this quiz!")
+playing = input("Do you want to play? ")
+if playing != "yes":
+    quit()
+
+print("okay lets play :)")    
+answer = input("What is hacking? ")
+if answer == "breaking systems or applications":
+    print("Correct!")
+else: 
+    print("Not Correct Try again!")    
+    
+answer = input("what is an api? ")
+if answer == "application programmming interface":
+    print("Correct!")
+else: 
+    print("Not Correct Try again!")
+    
+answer = input("What is the latest gpu out rn ? ")
+if answer == "rtx 3080":
+    print("Correct!")
+else: 
+    print("Not Correct Try again!")
+    
